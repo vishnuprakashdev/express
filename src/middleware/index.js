36c530c60e34a0218961c111c21fccc1a4bfb877
vishnuprakashdev <vishnuprakashdev@hotmail.com>
2025-06-24 21:01:27 +0530

    Update index.js

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -1,9 +1,11 @@
 import responseMiddleware from "./response.middleware.js";
 import validate from "./validate.js";
-import upload from './uploadMiddleware.js'
+import upload from './uploadMiddleware.js';
+import { errorHandler } from "./errorHandeler.js";
+
 
 
 
 export {
-    responseMiddleware, validate, upload
+    responseMiddleware, validate, upload, errorHandler
 }
